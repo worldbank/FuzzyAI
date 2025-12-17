7ec6434953940c7a4a672c4d9b4494bc9ab34c90
adrame1@ifc.org <adrame1@ifc.org>
2025-12-17 15:20:30 +0000

    typo

--- deduplix/__init__.py
+++ deduplix/__init__.py
@@ -1,16 +1,11 @@
 __version__ = "0.1.0"
 
-from .core import DeduplicationPipeline, DeduplicationResult,CrossDatasetResult
+from .core import DeduplicationPipeline, DeduplicationResult, CrossDatasetResult
 from .matchers import FuzzyMatcher
 from .validators import LLMValidator, RuleBasedValidator
 from .utils import (
-<<<<<<< HEAD
     load_config,
     create_sample_config,
-=======
-    load_config, 
-    create_sample_config, 
->>>>>>> c032314305f8e2afa96c70afb0030f76ad8c3a64
     remove_duplicates_after_deduplication,
     simple_remove_duplicates
 )
@@ -19,11 +14,7 @@ __all__ = [
     'DeduplicationPipeline',
     'DeduplicationResult',
     'CrossDatasetResult',
-<<<<<<< HEAD
     'FuzzyMatcher',
-=======
-    'FuzzyMatcher', 
->>>>>>> c032314305f8e2afa96c70afb0030f76ad8c3a64
     'LLMValidator',
     'RuleBasedValidator',
     'load_config',
